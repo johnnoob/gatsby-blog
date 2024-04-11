1d26503f938e72eaaf1bdedca1bc57b8d9e308a0
JohnGenius <johngenius@JohnGeniusdeMacBook-Pro.local>
2024-04-11 12:47:41 +0800

    17th push

--- src/components/TopPosts.js
+++ src/components/TopPosts.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "./Button";
 import PostCard from "./PostCardTopPosts";
 import { useStaticQuery, graphql, Link } from "gatsby";
@@ -33,9 +33,13 @@ const TopPosts = () => {
     );
   });
   const [cardIndex, setCardIndex] = useState(0);
+  const [translateClass, setTranslateClass] = useState("");
+  useEffect(() => {
+    setTranslateClass(`-translate-x-${1 * cardIndex}/3`);
+  }, [cardIndex]);
   const handleNextCard = () => {
     if (cardIndex < postCards.length - 1) {
-      setCardIndex(cardIndex + 1);
+      setCardIndex((cardIndex) => cardIndex + 1);
     } else {
       setCardIndex(0);
     }
@@ -44,7 +48,7 @@ const TopPosts = () => {
     if (cardIndex === 0) {
       setCardIndex(postCards.length - 1);
     } else {
-      setCardIndex(cardIndex - 1);
+      setCardIndex((cardIndex) => cardIndex - 1);
     }
   };
   return (
@@ -59,22 +63,19 @@ const TopPosts = () => {
       </div>
       <div className="w-full relative overflow-hidden">
         <button
-          className="bg-black text-white px-2 py-5 rounded-r-lg opacity-80 absolute top-1/2 z-10 left-0 -translate-y-1/2 lg:hidden"
+          className="bg-black text-white px-2 py-5 rounded-r-lg opacity-50 hover:opacity-90 absolute top-1/2 z-10 left-0 -translate-y-1/2 lg:hidden"
           onClick={handlePrevCard}
         >
           <BsChevronLeft size={20} />
         </button>
         <button
-          className="bg-black text-white px-2 py-5 rounded-l-lg opacity-80 absolute top-1/2 z-10 right-0 -translate-y-1/2 lg:hidden"
+          className="bg-black text-white px-2 py-5 rounded-l-lg opacity-50 hover:opacity-90 absolute top-1/2 z-10 right-0 -translate-y-1/2 lg:hidden"
           onClick={handleNextCard}
         >
           <BsChevronRight size={20} />
         </button>
         <div
-          className={`grid grid-cols-2 grid-rows-2 gap-3 max-lg:grid-cols-3 max-lg:gap-0 max-lg:grid-rows-1 max-lg:w-[300%] max-lg:-translate-x-${(
-            1 * cardIndex
-          ).toString()}/3 max-lg:transition-all`}
-          style={{}}
+          className={`grid grid-cols-2 grid-rows-2 gap-3 max-lg:grid-cols-3 max-lg:gap-0 max-lg:grid-rows-1 max-lg:w-[300%] max-lg: max-lg:${translateClass} transition`}
         >
           <div className="col-span-1 row-span-2 max-lg:row-span-1 max-lg:col-span-1">
             {postCards[0]}
