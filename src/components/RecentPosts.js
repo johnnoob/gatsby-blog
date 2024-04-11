ec3413da30a667077b43da1d9a1c7bd4179428b4
s079053002006 <s079053002006@gmail.com>
2024-04-12 07:24:10 +0800

    20th commit

--- src/components/RecentPosts.js
+++ src/components/RecentPosts.js
@@ -1,14 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Button from "./Button";
 import PostCard from "./PostCard";
 import { useStaticQuery, graphql, Link } from "gatsby";
 import { getImage } from "gatsby-plugin-image";
 
 const RecentPosts = () => {
-  let list = [];
-  for (let i = 0; i < 8; i++) {
-    list.push(<PostCard key={i} />);
-  }
+  const numOfPagePosts = 3;
+  const [page, setPage] = useState(1);
   const data = useStaticQuery(graphql`
     query {
       allImageSharp {
@@ -20,23 +18,46 @@ const RecentPosts = () => {
       }
     }
   `);
-  console.log(data.allImageSharp.edges);
-  const images = data.allImageSharp.edges.map((edge) => getImage(edge.node));
-  const postCards = images.map((imageUrl) => (
+  const posts = data.allImageSharp.edges;
+  const numOfPosts = data.allImageSharp.edges.length;
+  const numOfPages = Math.ceil(numOfPosts / numOfPagePosts);
+  const pagesArray = Array.from({ length: numOfPages }, (_, i) => i + 1);
+  const pagePosts = posts.slice(
+    numOfPagePosts * (page - 1),
+    numOfPagePosts * page
+  );
+  console.log(pagePosts);
+  const handleChangePage = (page) => {
+    setPage(page);
+  };
+  const images = pagePosts.map((edge) => getImage(edge.node));
+  const postCards = images.map((imageUrl, index) => (
     <PostCard
-      key={imageUrl.backgroundColor}
+      key={index}
       imageUrl={imageUrl}
       bgColor={imageUrl.backgroundColor}
     />
   ));
+  console.log(postCards.length);
   return (
-    <div className="">
+    <div>
       <div className="flex justify-between items-center mb-8">
         <h1 className="text-3xl font-bold">近期文章</h1>
         <Button isBlack={true}>觀看全部</Button>
       </div>
       <div className="grid grid-cols-3 gap-8 max-lg:grid-cols-1 max-lg:gap-4">
         {postCards}
+      </div>
+      <div>
+        {pagesArray.map((page, index) => {
+          return (
+            <>
+              <button key={index} onClick={() => handleChangePage(index + 1)}>
+                {index + 1}
+              </button>
+            </>
+          );
+        })}
       </div>
     </div>
   );
