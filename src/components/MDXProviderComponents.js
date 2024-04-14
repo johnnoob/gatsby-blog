cd239d3c75ed18e892e31b6236f57c4dd6fab43c
s079053002006 <s079053002006@gmail.com>
2024-04-15 07:33:07 +0800

    22th commit

---
+++ src/components/MDXProviderComponents.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const h1 = (props) => (
+  <h1
+    className=" text-2xl leading-relaxed font-semibold flex items-center"
+    {...props}
+  />
+);
+const h2 = (props) => (
+  <h1 className=" text-xl leading-relaxed font-semibold" {...props} />
+);
+const h3 = (props) => (
+  <h1 className=" text-lg leading-relaxed font-semibold" {...props} />
+);
+const p = (props) => <p className=" leading-relaxed font-light" {...props} />;
+
+export const MDXProviderComponents = {
+  h1: h1,
+  h2: h2,
+  h3: h3,
+  p: p,
+};
--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,61 +1,151 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import Layout from "../../components/layout.js";
 import Seo from "../../components/Seo.js";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 import { MDXProvider } from "@mdx-js/react";
-import { FaListUl } from "react-icons/fa6";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { BsListTask, BsFillTagFill } from "react-icons/bs";
 import { useState } from "react";
-
-const h1 = (props) => <h1 className=" text-2xl leading-normal" {...props} />;
-const p = (props) => (
-  <p className=" leading-relaxed font-light text-lg" {...props} />
-);
-
-const components = {
-  h1: h1,
-  p: p,
-};
+import { MDXProviderComponents } from "../../components/MDXProviderComponents.js";
+import { FaAngleRight, FaAngleDown } from "react-icons/fa6";
+import Button from "../../components/Button.js";
 
 const BlogPost = ({ data, children }) => {
-  const [isExpand, setIsExpand] = useState(true);
-  const handleExpandClick = () => {
-    setIsExpand(!isExpand);
-    console.log("Hello World");
+  const { date, slug, tags, hero_image } = data.mdx.frontmatter;
+  // const { items: contents } = data.mdx.tableOfContents;
+  const heroImage = getImage(hero_image.childrenImageSharp[0]);
+  const { items: contents } = data.mdx.tableOfContents;
+
+  let initialIsOpenMap = {};
+  contents.forEach((_, index) => {
+    initialIsOpenMap[index] = false;
+  });
+
+  const [isOpenMap, setIsOpenMap] = useState(initialIsOpenMap);
+  // const [current]
+  const handleH1Open = (index) => {
+    setIsOpenMap((isOpenMap) => ({ ...isOpenMap, [index]: !isOpenMap[index] }));
   };
+
+  const contentsRef = useRef(null);
+  function getMap() {
+    if (!contentsRef.current) {
+      // Initialize the Map on first usage.
+      contentsRef.current = new Map();
+    }
+    return contentsRef.current;
+  }
+  // function scrollToId(itemId) {
+  //   const map = getMap();
+  //   const node = map.get(itemId);
+  //   node.scrollIntoView({
+  //     behavior: "smooth",
+  //     block: "nearest",
+  //     inline: "center",
+  //   });
+  // }
+  console.log(contentsRef);
+
   return (
     <Layout>
-      <section className="pt-32">
-        <section className="flex justify-center items-start max-md:flex-col text-gray-500">
-          <aside className="flex flex-col items-start gap-3 px-5 text-base py-2 bg-white text-gray-700 sticky top-0">
-            <button
-              className="flex gap-[5px] px-3 py-2 hover:bg-gray-100 rounded-xl"
-              onClick={handleExpandClick}
-            >
-              <FaListUl className="text-lg mt-1" />
-              <div>目錄</div>
-            </button>
-            <div
-              className={`w-72 pl-[20px] text-base ${isExpand ? "" : "hidden"}`}
-            >
+      <section className="pt-40 post-max-container">
+        <section className="padding-r flex justify-start items-start gap-10 max-lg:flex-col max-lg:padding-l">
+          <aside className="flex flex-col items-start gap-3 text-base sticky max-lg:relative top-0 basis-[300px]">
+            <div className={`text-base`}>
+              <div className="relative flex justify-center mb-4">
+                <div className="flex justify-center items-center px-[20px] gap-1 bg-white">
+                  <BsListTask size={20} />
+                  <h4 className=" tracking-wider">目錄</h4>
+                </div>
+                <div className="absolute top-1/2 h-[2px] w-full bg-black -translate-y-1/2 -z-10"></div>
+              </div>
               <ul className="flex flex-col text-gray-500">
-                <li className="pl-[14px] border-l-[1px] border-gray-300 hover:border-orange-500 hover:text-orange-500 py-2">
-                  <div className>
-                    SORA引起translate-SORA引起translate-SORA引起translate-
-                    SORA引起translate- SORA引起translate- SORA引起translate-
-                    SORA引起translate- SORA引起translate- SORA引起translate-
-                  </div>
-                </li>
-                <li className="pl-[14px] border-l-[1px] border-gray-300 hover:border-orange-500 hover:text-orange-500 py-2">
-                  <div className>SORA引起translate</div>
-                </li>
-                <li className="pl-[14px] border-l-[1px] border-gray-300 hover:border-orange-500 hover:text-orange-500 py-2">
-                  <div className>SORA引起translate</div>
-                </li>
+                {contents.map((content, index) => {
+                  const { title: h1, url: h1Url, items: h2s = [] } = content;
+                  return (
+                    <li key={h1} className="border-gray-300 py-1">
+                      <div className="flex justify-start items-center gap-2 pb-1">
+                        {h2s && (
+                          <button onClick={() => handleH1Open(index)}>
+                            {isOpenMap[index] ? (
+                              <FaAngleDown size={15} />
+                            ) : (
+                              <FaAngleRight size={15} />
+                            )}
+                          </button>
+                        )}
+                        <a
+                          href={h1Url}
+                          ref={(node) => {
+                            const map = getMap();
+                            if (node) {
+                              map.set(index, node);
+                            } else {
+                              map.delete(index);
+                            }
+                          }}
+                        >
+                          {h1}
+                        </a>
+                      </div>
+                      {h2s && (
+                        <ul className="">
+                          {h2s.map(({ title: h2, url: h2Url }) => {
+                            return (
+                              <li
+                                key={h2}
+                                className={`${
+                                  isOpenMap[index] ? "" : "hidden"
+                                } ml-[7px] pl-[16px] py-[2px] border-l-[1px] border-slate-300 hover:border-black hover:text-black`}
+                              >
+                                <a href={h2Url}>{h2}</a>
+                              </li>
+                            );
+                          })}
+                        </ul>
+                      )}
+                    </li>
+                  );
+                })}
               </ul>
             </div>
+            <div className="w-full">
+              <div className="relative flex justify-center mb-4">
+                <div className="flex justify-center items-center bg-white px-[20px] gap-1">
+                  <BsFillTagFill size={20} />
+                  <h4 className=" tracking-wider">文章標籤</h4>
+                </div>
+                <div className="absolute top-1/2 h-[2px] w-full bg-black -translate-y-1/2 -z-10"></div>
+              </div>
+              <div className="flex flex-wrap gap-2 mt-2">
+                <Link to="/" className="px-3 py-1 bg-slate-200 rounded-md">
+                  AI
+                </Link>
+                <Link className="px-3 py-1 bg-slate-200 rounded-md">
+                  Python
+                </Link>
+                <Link className="px-3 py-1 bg-slate-200 rounded-md">
+                  Javascript
+                </Link>
+                <Link className="px-3 py-1 bg-slate-200 rounded-md">旅遊</Link>
+                <Link to="/" className="px-3 py-1 bg-slate-200 rounded-md">
+                  AI
+                </Link>
+                <Link className="px-3 py-1 bg-slate-200 rounded-md">
+                  asasddasda
+                </Link>
+                <Link className="px-3 py-1 bg-slate-200 rounded-md">zxczx</Link>
+                <Link className="px-3 py-1 bg-slate-200 rounded-md">asd</Link>
+                <Link className="px-3 py-1 bg-slate-200 rounded-md">
+                  asdasdasda
+                </Link>
+              </div>
+            </div>
           </aside>
-          <article className="flex-1 max-w-4xl">
-            <MDXProvider components={components}>{children}</MDXProvider>
+          <article className="flex-1">
+            <MDXProvider components={MDXProviderComponents}>
+              {children}
+            </MDXProvider>
           </article>
         </section>
       </section>
@@ -66,9 +156,16 @@ const BlogPost = ({ data, children }) => {
 export const query = graphql`
   query ($id: String) {
     mdx(id: { eq: $id }) {
+      tableOfContents(maxDepth: 2)
       frontmatter {
-        date
-        title
+        date(formatString: "MMMM D, Y")
+        slug
+        hero_image {
+          childrenImageSharp {
+            gatsbyImageData
+          }
+        }
+        tags
       }
     }
   }
