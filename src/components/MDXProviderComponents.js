f76f5b5055ab5dd8ecec155ba184a218eab7289a
JohnGenius <johngenius@JohnGeniusdeMacBook-Pro.local>
2024-04-17 17:48:27 +0800

    29th commit

--- src/components/MDXProviderComponents.js
+++ src/components/MDXProviderComponents.js
@@ -1,22 +1,35 @@
 import React from "react";
 
 const h1 = (props) => (
-  <h1
-    className=" text-2xl leading-relaxed font-semibold flex items-center"
+  <h1 className="text-3xl leading-relaxed font-semibold py-2" {...props} />
+);
+const h2 = (props) => (
+  <h2 className="text-2xl leading-relaxed font-semibold py-1" {...props} />
+);
+const h3 = (props) => (
+  <h3 className="text-xl leading-relaxed font-semibold py-1" {...props} />
+);
+const p = (props) => (
+  <p
+    className="text-lg leading-loose text-gray-p tracking-wider font-light"
     {...props}
   />
 );
-const h2 = (props) => (
-  <h2 className="text-xl leading-relaxed font-semibold" {...props} />
-);
-const h3 = (props) => (
-  <h3 className="text-lg leading-relaxed font-semibold" {...props} />
-);
-const p = (props) => <p className="leading-relaxed font-light" {...props} />;
+const ol = (props) => {
+  <ol style={{ color: "red" }} {...props} />;
+};
+const ul = (props) => {
+  <ul className="list-disc" {...props} />;
+};
+const li = (props) => {
+  <li className="text-red-300" {...props} />;
+};
 
 export const MDXProviderComponents = {
   h1: h1,
   h2: h2,
   h3: h3,
   p: p,
+  ol: ol,
+  ul: ul,
 };
